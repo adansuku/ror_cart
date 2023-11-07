facf344a118f33d00cec46579671309874a74204
adansuku <adangrx@gmail.com>
2023-11-07 22:51:05 +0000

    added product controller and views

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,9 @@
+class ProductsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @products = Product.all
+    @cart_items = current_cart.cart_items
+    @number_of_cart_items = @cart_items.sum(&:quantity) || 0
+  end
+end
