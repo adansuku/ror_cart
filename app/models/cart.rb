7073398ce1eb2e7729486584a02a6bdb4be5ec67
adansuku <adangrx@gmail.com>
2023-11-08 18:38:45 +0000

    updated cart functionality

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,8 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   belongs_to :user
 
+  validates :user_id, uniqueness: true
+
   def add_product(product, quantity = 1)
     item = cart_items.find_or_initialize_by(product: product)
     item.quantity = (item.quantity || 0) + quantity
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,15 +4,28 @@ class CartItem < ApplicationRecord
   belongs_to :product
 
   delegate :discount, to: :product, allow_nil: true
-  before_save :apply_discount
+  before_save :apply_and_save_discount
+
+  def apply_and_save_discount
+    if discount
+      apply_discount
+    else
+      self.total_price = product.price * quantity
+    end
+  end
 
   private
 
   def apply_discount
-    self.total_price = if discount
-                         discount.calculate_discounted_price(product.price, quantity)
-                       else
-                         product.price * quantity
-                       end
+    self.total_price = calculate_discounted_price
+    self.discount_amount = original_price - total_price
+  end
+
+  def original_price
+    product.price * quantity
+  end
+
+  def calculate_discounted_price
+    discount.calculate_discounted_price(product.price, quantity)
   end
 end
