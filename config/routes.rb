634b90ebddf12574712a84f506254ea6a11eaa4f
adansuku <adangrx@gmail.com>
2023-11-08 18:35:45 +0000

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'products/show'
+  get 'products/create'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
+  resources :cart_items, only: [:update], as: 'update_cart_items'
+  resources :cart_items, only: [:destroy], as: 'destroy_cart_items'
+
+  get 'cart/empty_cart', to: 'carts#empty_cart', as: 'clear_cart_items'
+  post '/add_to_cart/:product_id', to: 'carts#add_to_cart', as: 'add_to_cart'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+    # confirmations: 'users/confirmations',
+    # unlocks: 'users/unlocks',
+    # omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
+  # Default root
+  root 'products#show'
 end
